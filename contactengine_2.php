dfd9dbe74cf8178203807d3348c7276f1caffe07
Dominik S. Ting <dt101677@gmail.com>
2016-06-26 02:21:57 -0500

    Update contact engine.

--- contactengine_2.php
+++ contactengine_2.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 $EmailFrom = "dominikting@gmail.com";
 $EmailTo = "dt101677@gmail.com";
 $Subject = "Nice & Simple Contact Form by CSS-Tricks";
@@ -16,40 +14,6 @@ foreach ($form_fields as $field) {
   $$field = Trim( stripslashes( $_POST[$field] ) )
 }
 
-/*
-$Name = Trim(stripslashes($_POST['Name'])); 
-$Tel = Trim(stripslashes($_POST['Tel'])); 
-$Email = Trim(stripslashes($_POST['Email'])); 
-$Message = Trim(stripslashes($_POST['Message'])); 
-*/
-
-// validation
-/*
-$validationOK=true;
-if (!$validationOK) {
-  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
-  exit;
-}
-*/
-
-// prepare email body text
-/*
-$Body = "";
-$Body .= "Name: ";
-$Body .= $Name;
-$Body .= "\n";
-$Body .= "Tel: ";
-$Body .= $Tel;
-$Body .= "\n";
-$Body .= "Email: "; 
-$Body .= $Email;
-$Body .= "\n";
-$Body .= "Message: ";
-$Body .= $Message;
-$Body .= "\n";
-*/
-
-// form_fields = [ { "Name": "Value" } ];
 $fields = [
   "Name"    => $Name,
   "Tel"     => $Tel,
@@ -57,11 +21,6 @@ $fields = [
   "Message" => $Message
 ];
 
-// form_fields.forEach(function(item) {
-//   for ( var key in item ) {
-//     console.log( key + ": " + item[key] + "\n" );
-//   }
-// });
 $Body = "";
 foreach ($fields as $label => $input) {
   $Body .= "{$label}: {$input}\n";
@@ -73,8 +32,7 @@ $success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
 // redirect to success page 
 if ($success){
   print "<meta http-equiv=\"refresh\" content=\"0;URL=contactthanks.php\">";
-}
-else{
+} else {
   print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
 }
 ?>
